1dd8bf33ac030d770b9b592c9fafcf58b08d96a6
Laura Matiukievic <laura.matiukievic@gmail.com>
2023-06-24 14:56:26 +0300

    added users

--- src/pages/users/Users.js
+++ src/pages/users/Users.js
@@ -1,7 +1,39 @@
 import React from "react";
+import axios from "axios";
+
+import { API_URL } from "../../config";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import Container from "../../components/container/Container";
 
 function Users() {
-  return <div>Vartotojai</div>;
+  const [users, setUsers] = useState([]);
+
+  async function fetchData() {
+    const res = await axios.get(`${API_URL}/users?_embed=orders`);
+    setUsers(res.data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <Container>
+      <div className="users-container">
+        <ul>
+          {users.map((user) => (
+            <li className="user-name" key={user.id}>
+              <Link className="users-link" to={`/users/${user.id}`}>
+                {user.name}(Užsakymų skaičius {user.orders.length})
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Container>
+  );
 }
 
 export default Users;
