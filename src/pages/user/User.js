c680da9b18d055c46b08292b95df5ce648fbe994
Laura Matiukievic <laura.matiukievic@gmail.com>
2023-06-24 13:25:13 +0300

    added pages and routes

---
+++ src/pages/user/User.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function User() {
+  return <div>Vienas vartotojas</div>;
+}
+
+export default User;
---
+++ src/pages/users/Users.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Users() {
+  return <div>Vartotojai</div>;
+}
+
+export default Users;
