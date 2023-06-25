4b65ff8bccb43a933f2bc99661fd6c42a51bb8b1
Laura Matiukievic <laura.matiukievic@gmail.com>
2023-06-25 21:48:25 +0300

    added edit to service

--- src/pages/service/Service.js
+++ src/pages/service/Service.js
@@ -8,10 +8,13 @@ import { RingLoader } from "react-spinners";
 
 import Container from "../../components/container/Container";
 import { useParams } from "react-router";
+import ServiceForm from "../../components/serviceForm/ServiceForm";
+import { Button } from "@mui/base";
 
 function Service() {
   const { id } = useParams();
   const [service, setService] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
 
   async function fetchData() {
     const res = await axios.get(`${API_URL}/services/${id}`);
@@ -25,14 +28,21 @@ function Service() {
   if (!service) {
     return <RingLoader color="rgba(214, 142, 54, 1)" />;
   }
+  const editService = () => {
+    fetchData();
+    setIsEdit(false);
+  };
 
   return (
     <Container>
+      {isEdit && <ServiceForm service={service} onEdit={editService} />}
       <div className="service-info" key={service.id}>
         <h2>Paslaugos pavadinimas: {service.title}</h2>
         <p>Paslaugos aprašymas:{service.body}</p>
         <p>Paslaugos kaina: {service.price} e.</p>
       </div>
+
+      {!isEdit && <Button onClick={() => setIsEdit(true)}>Koreguoti paslaugą</Button>}
     </Container>
   );
 }
